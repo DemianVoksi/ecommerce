6f6a38d52cc5275d9bc2d923b91920257b7fa948
Demian Voksi <demian.voksi@gmail.com>
2023-04-16 13:27:11 +0200

    filter

--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -1,5 +1,20 @@
 import './filter.css';
 
+/*
+Filters: 
+
+- producer (checkbox)
+- price (collapse from-to)
+- screen size (checkbox)
+- cpu (checkbox)
+- os (checkbox)
+- ram (collapse from-to)
+- storage (collapse from-to)
+
+Make an object of all the queries and build the query from there
+
+*/
+
 export const Filter = () => {
 	return <div className="filter-wrapper"></div>;
 };
