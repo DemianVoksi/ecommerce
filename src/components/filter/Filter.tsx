0ba0dbbc2b1c3fef1ccc9a629e451527fdfb3dbf
Demian Voksi <demian.voksi@gmail.com>
2023-04-23 17:58:20 +0200

    filter accordion layout

--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import {
+	MdKeyboardArrowDown,
+	MdKeyboardArrowUp,
 	MdKeyboardDoubleArrowLeft,
 	MdKeyboardDoubleArrowRight
 } from 'react-icons/md';
@@ -22,23 +24,110 @@ Make an object of all the queries and build the query from there
 
 export const Filter = () => {
 	const [visibility, setVisibility] = useState(false);
+	const [producerVisibility, setProducerVisibility] = useState(false);
+	const [priceVisibility, setPriceVisibility] = useState(false);
+	const [screenVisibility, setScreenVisibility] = useState(false);
+	const [cpuVisibility, setCpuVisibility] = useState(false);
+	const [osVisibility, setOsVisibility] = useState(false);
+	const [ramVisibility, setRamVisibility] = useState(false);
+	const [storageVisibility, setStorageVisibility] = useState(false);
 
 	return (
 		<div className={visibility ? 'filter-wrapper' : 'filter-wrapper-hide'}>
 			<div>
 				<div className="icon-wrapper">
 					{visibility ? (
-						<MdKeyboardDoubleArrowLeft
-							size={40}
-							onClick={() => setVisibility(!visibility)}
-						/>
+						<div className="filter-p">
+							<p className="para">filter</p>
+							<MdKeyboardDoubleArrowLeft
+								size={40}
+								onClick={() => setVisibility(!visibility)}
+							/>
+						</div>
 					) : (
-						<MdKeyboardDoubleArrowRight
-							size={40}
-							onClick={() => setVisibility(!visibility)}
-						/>
+						<div className="filter-p">
+							<MdKeyboardDoubleArrowRight
+								size={40}
+								onClick={() => setVisibility(!visibility)}
+							/>
+							<p className="para">filter</p>
+						</div>
 					)}
 				</div>
+			</div>
+			<div
+				className={
+					visibility ? 'filter-categories' : 'filter-categories-hide'
+				}
+			>
+				<form className="filter-form">
+					<div className="fcategory-wrapper">
+						<div
+							className="fcategory-show-wrapper"
+							onClick={() =>
+								setProducerVisibility(!producerVisibility)
+							}
+						>
+							<p className="fcategory-p">producer</p>
+							<div className="fcategory-icon">
+								{producerVisibility ? (
+									<MdKeyboardArrowUp size={30} />
+								) : (
+									<MdKeyboardArrowDown size={30} />
+								)}
+							</div>
+						</div>
+						<div
+							className={
+								producerVisibility
+									? 'fcategory-checkboxes'
+									: 'fcategory-checkboxes-hide'
+							}
+						>
+							<div className="single-input">
+								<input
+									type="checkbox"
+									id="apple"
+									name="Apple"
+								/>
+								<label htmlFor="apple">Apple</label>
+							</div>
+							<div className="single-input">
+								<input type="checkbox" id="Dell" name="Dell" />
+								<label htmlFor="Dell">Dell</label>
+							</div>
+
+							<div className="single-input">
+								<input
+									type="checkbox"
+									id="lenovo"
+									name="Lenovo"
+								/>
+								<label htmlFor="lenovo">Lenovo</label>
+							</div>
+
+							<div className="single-input">
+								{' '}
+								<input type="checkbox" id="hp" name="HP" />
+								<label htmlFor="hp">HP</label>
+							</div>
+
+							<div className="single-input">
+								<input type="checkbox" id="acer" name="Acer" />
+								<label htmlFor="acer">Acer</label>
+
+								<div className="single-input">
+									<input
+										type="checkbox"
+										id="asus"
+										name="Asus"
+									/>
+									<label htmlFor="asus">Asus</label>
+								</div>
+							</div>
+						</div>
+					</div>
+				</form>
 			</div>
 		</div>
 	);
